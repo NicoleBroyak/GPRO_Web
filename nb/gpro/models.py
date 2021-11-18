a04a7c572f52ad1d3b6410aa61d9a1c26e06b972
Nikola Broyak <nikolabroyak@gmail.com>
2021-11-18 17:32:53 +0100

    Addition of models Season and Race in models.py

--- nb/gpro/models.py
+++ nb/gpro/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Season(models.Model):
+    season_number = models.IntegerField()
+
+class Race(models.Model):
+    race_number = models.IntegerField()
+    track = models.CharField(max_length=30)
+    race_date = models.DateField()
+    season_number = models.ForeignKey(Season, on_delete=CASCADE)
